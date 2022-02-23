cd3f2d0dd94bd054549901c4010e7c52b48ec63d
DNA talent <dna0187@gmail.com>
2022-02-23 23:19:30 +0200

    make it better223

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
-
+import path from 'path';
 const app = express();
 dotenv.config();
 
@@ -18,10 +18,10 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use( express.static(path.join(__dirname, 'client/build')));
+    app.use( express.static( 'client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.js"))
+        res.sendFile(path.resolve( __dirname, "client", "build", "index.js"))
     })
 }
 
