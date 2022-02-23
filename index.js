c962927be21c5bbe226703697e325fd63f2712a9
DNA talent <dna0187@gmail.com>
2022-02-23 20:18:41 +0200

    make it better22

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use( express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.js"))
+    })
+}
 
 app.get('/', (req, res) => {
     res.send('APP IS RUNING');
