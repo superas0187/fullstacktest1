7507b7948f5e0c98a25f7ac4e59cb1e0b862ad77
DNA talent <dna0187@gmail.com>
2022-02-24 10:33:13 +0200

    make it better223

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 
 
-app.use( express.json({ limit: '30mb', extended: true }));
-app.use( express.urlencoded({ limit: '30mb', extended: true }));
+app.use( express.json({ extended: true }));
+app.use( express.urlencoded({  extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
